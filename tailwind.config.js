e110d4e9f9028aa586643d03faf1d79ad73a37a6
Sumit Sharma <sumitsh003@gmail.com>
2023-03-08 23:08:50 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'sans': ['Tenor Sans'],
+      'serif': ['Rozha One'],
+      'mono': ['Cutive Mono'],
+      'display': ['Abril Fatface'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
